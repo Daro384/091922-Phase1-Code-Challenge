bc38238a25f802ebbb430cea11a81fc00f9850fc
Joseph <joffan98@gmail.com>
2022-09-29 11:44:43 -0500

    finished Bonus

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 const api = "http://localhost:3000/characters"
 
+
 const addingCuties = cuteObject => {
     const cutie = document.createElement("span")
     cutie.textContent = cuteObject.name
 
-    cutie.addEventListener("click", event => {
-        document.getElementById("name").textContent = cuteObject.name
-        document.getElementById("image").src = cuteObject.image
-        document.getElementById("image").alt = cuteObject.name
-        document.getElementById("vote-count").textContent = cuteObject.votes
-    })
+    cutie.addEventListener("click", event => showCutieCenter(cuteObject))
     document.getElementById("character-bar").append(cutie)
 }
+
+
+const showCutieCenter = cutieObject => {
+    document.getElementById("name").textContent = cutieObject.name
+    document.getElementById("image").src = cutieObject.image
+    document.getElementById("image").alt = cutieObject.name
+    document.getElementById("vote-count").textContent = cutieObject.votes
+}
+
 
 const voting = () => {
     const addVotesButton = document.getElementById("votes-form")
@@ -21,8 +26,29 @@ const voting = () => {
         currentVotes += parseInt(event.target.votes.value)
         document.getElementById("vote-count").textContent = currentVotes
     })
-    
+
+    const resetVotesButton = document.getElementById("reset-btn")
+    resetVotesButton.addEventListener("click", e => {
+        document.getElementById("vote-count").textContent = 0
+    })
 }
+
+
+const addNewCutie = () => {
+    const addCharacterForm = document.getElementById("character-form")
+    addCharacterForm.addEventListener("submit", event => {
+        event.preventDefault()
+        const newCutieObject = {
+            name:event.target.name.value,
+            image:event.target["image-url"].value,
+            votes:0,
+        }
+        addingCuties(newCutieObject)
+        showCutieCenter(newCutieObject)
+    })
+
+}
+
 
 const renderPage = () => {
     fetch(api).then(res => res.json())
@@ -30,8 +56,11 @@ const renderPage = () => {
         cuteObjects.forEach(cuteObject => {
             addingCuties(cuteObject)
         })
+        showCutieCenter(cuteObjects[0])
     })
 }
 
+
 renderPage()
 voting()
+addNewCutie()
