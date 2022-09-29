9c1709bb788bc39cc04d58041981ee9991fd0252
Joseph <joffan98@gmail.com>
2022-09-29 11:25:32 -0500

    core deliverables

--- src/index.js
+++ src/index.js
@@ -3,7 +3,25 @@ const api = "http://localhost:3000/characters"
 const addingCuties = cuteObject => {
     const cutie = document.createElement("span")
     cutie.textContent = cuteObject.name
+
+    cutie.addEventListener("click", event => {
+        document.getElementById("name").textContent = cuteObject.name
+        document.getElementById("image").src = cuteObject.image
+        document.getElementById("image").alt = cuteObject.name
+        document.getElementById("vote-count").textContent = cuteObject.votes
+    })
     document.getElementById("character-bar").append(cutie)
+}
+
+const voting = () => {
+    const addVotesButton = document.getElementById("votes-form")
+    addVotesButton.addEventListener("submit", event => {
+        event.preventDefault()
+        let currentVotes = parseInt(document.getElementById("vote-count").textContent)
+        currentVotes += parseInt(event.target.votes.value)
+        document.getElementById("vote-count").textContent = currentVotes
+    })
+    
 }
 
 const renderPage = () => {
@@ -16,3 +34,4 @@ const renderPage = () => {
 }
 
 renderPage()
+voting()
