130f9a528d5e89a321b1b6c9a260a374ec93ba93
Joseph <joffan98@gmail.com>
2022-09-29 11:11:06 -0500

    making sure i did everything correct

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-// Your code here
+const api = "http://localhost:3000/characters"
+
+const addingCuties = cuteObject => {
+    const cutie = document.createElement("span")
+    cutie.textContent = cuteObject.name
+    document.getElementById("character-bar").append(cutie)
+}
+
+const renderPage = () => {
+    fetch(api).then(res => res.json())
+    .then(cuteObjects => {
+        cuteObjects.forEach(cuteObject => {
+            addingCuties(cuteObject)
+        })
+    })
+}
+
+renderPage()
